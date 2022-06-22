391603c42b72c58b2bac143aaeb8c730a3f7dd7e
CardenasGalarza <alex10estadistica@gmail.com>
2022-06-22 00:53:48 -0500

    cffd

--- bandeja.py
+++ bandeja.py
@@ -6,7 +6,7 @@ import database as db
 
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:")# layout="wide"
 
 
 # --- USER AUTHENTICATION ---
