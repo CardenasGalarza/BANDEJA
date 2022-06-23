302342a1883518a06da7247f2bedcd106d674d16
CardenasGalarza <alex10estadistica@gmail.com>
2022-06-23 02:52:03 -0500

    rfrfrf

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 mkdir -p ~/.streamlit/
-echo "[general]  
-email = \"alex10estadistica@gmail.com\""  > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = true"  >> ~/.streamlit/config.toml
+
+echo "\
+[general]\n\
+email = \"alex10estadistica@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
