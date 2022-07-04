5750b50dbb6e09bfccacd06dcf6c01382101aec3
CardenasGalarza <alex10estadistica@gmail.com>
2022-07-04 00:46:45 -0500

    ee33

--- setup.sh
+++ setup.sh
@@ -5,14 +5,7 @@ echo "\
 email = \"alex10estadistica@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[theme]\n\
-base='light'\n\
-primaryColor='#3a70ec'\n\
-backgroundColor='#5f4bd8'\n\
-secondaryBackgroundColor = '#F0F2F6'\n\
-textColor='#0e1862'\n\
-font = 'sans serif'\n\
+
 
 [server]\n\
 headless = true\n\
